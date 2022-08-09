d45c7ce5c62fac215824d4cf2524412383f14b44
cahito <cahito@gmail.com>
2022-08-09 18:41:06 -0300

    Feat - Requisito cinco cumprido

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,41 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  protected _name: string;
+  protected _special: number;
+  protected _cost: number;
+  private _energy: EnergyType;
+  static _createdArchetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+    this._energy = 'mana';
+    Necromancer._createdArchetypeInstances += 1;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._createdArchetypeInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+}
+
+export default Necromancer;
